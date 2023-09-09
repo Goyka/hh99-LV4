2aee9de3de491b4600d4b37daac290f17794f2d6
hongmikyeong <mikyeong.dev@gmail.com>
2023-09-09 11:42:20 +0900

    개발 환경 구성

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: "baseURL",
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,5 @@
+import * as St from "styled-components";
+
+export default function Modal() {
+  return <></>;
+}
