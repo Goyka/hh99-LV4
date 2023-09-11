6aad8784720f1cf6e2474981030df8cf6dfc5cd6
hongmikyeong <mikyeong.dev@gmail.com>
2023-09-11 13:42:46 +0900

    회원가입 페이지 UI 구현

--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -1,5 +1,27 @@
-import * as St from "styled-components";
+import * as St from "../styles/styles";
 
 export default function JoinPage() {
-  return <></>;
+  return (
+    <St.Container>
+      <St.Header>
+        <img src="img/lycos.webp" alt="img" style={{ width: "300px" }} />
+        <St.Title></St.Title>
+      </St.Header>
+      <St.Col margin="10px 0 20px">
+        <div>아이디</div>
+        <St.Input />
+      </St.Col>
+      <St.Col margin="10px 0 20px">
+        <div>비밀번호</div>
+        <St.Input />
+      </St.Col>
+      <St.Col margin="10px 0 40px">
+        <div>비밀번호 재확인</div>
+        <St.Input />
+      </St.Col>
+      <div>
+        <St.Button buttontheme="secondary">회원가입</St.Button>
+      </div>
+    </St.Container>
+  );
 }
